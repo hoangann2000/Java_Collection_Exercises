6305c0c1a1337e7b0e3a455ab3b02b26f73b5eba
unknown <hoangann2312@gmail.com>
2021-05-24 20:36:13 +0700

    first commit

---
+++ src/ArrayList_Exercises/ex1.java
@@ -0,0 +1,14 @@
+package ArrayList_Exercises;
+
+import java.util.*;
+public class ex1 {
+  public static void main(String[] args) {
+  List<String> list_Strings = new ArrayList<String>();
+  list_Strings.add("Red");
+  list_Strings.add("Green");
+  list_Strings.add("Orange");
+  list_Strings.add("White");
+  list_Strings.add("Black");
+  System.out.println(list_Strings);
+ }
+}
